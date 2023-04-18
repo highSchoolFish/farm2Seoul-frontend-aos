9d16524a3924534dc0716e3d7c6ba1e66408f365
Sangyoon <qlrqod123123@naver.com>
2023-04-19 01:21:11 +0900

    fix : viewPager 임시 보류 (프래그먼트 동적 높이 변경 필요)

--- app/src/main/java/com/farm2seoul_frontend_aos/MainActivity.kt
+++ app/src/main/java/com/farm2seoul_frontend_aos/MainActivity.kt
@@ -2,8 +2,13 @@ package com.farm2seoul_frontend_aos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.core.graphics.drawable.toDrawable
+import androidx.core.view.get
 import androidx.core.widget.NestedScrollView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.farm2seoul_frontend_aos.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
@@ -30,17 +35,24 @@ class MainActivity : AppCompatActivity() {
         val tabName = listOf("일별경매", "가게별 정보", "즐겨찾기")
         val tabIcon = listOf(R.drawable.daily_auction, R.drawable.store_info, R.drawable.star)
 
-        viewPagerAdapter = FragmentViewPagerAdapter(this)
+        /*viewPagerAdapter = FragmentViewPagerAdapter(this)
         viewPagerAdapter.fragments.addAll(fragmentList)
         binding.fragmentLayout.adapter = viewPagerAdapter
 
         TabLayoutMediator(binding.tabLayout, binding.fragmentLayout) {tab, pos ->
             tab.text = tabName[pos]
             tab.setIcon(tabIcon[pos])
-        }.attach()
+        }.attach()*/
+
+        replaceView(fragmentList[0])
 
         binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
             override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab?.position) {
+                    0 -> replaceView(fragmentList[0])
+                    1 -> replaceView(fragmentList[1])
+                    2 -> replaceView(fragmentList[2])
+                }
             }
 
             override fun onTabUnselected(tab: TabLayout.Tab?) {
@@ -50,5 +62,13 @@ class MainActivity : AppCompatActivity() {
                 binding.scrollLayout.fullScroll(NestedScrollView.FOCUS_UP)
             }
         })
+    }
+
+    private fun replaceView(tab: Fragment) {
+        var selectedFragment: Fragment? = null
+        selectedFragment = tab
+        selectedFragment?.let {
+            supportFragmentManager.beginTransaction().replace(R.id.fragment_layout, it).commit()
+        }
     }
 }
